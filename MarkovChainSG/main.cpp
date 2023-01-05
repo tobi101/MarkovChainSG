283a9197c3e8a6bb48826620ad838c0649505761
Sergey Reshetnikov <sreshetnikov101@gmail.com>
2023-01-05 03:00:56 +0300

    Добавьте файлы проекта.

---
+++ MarkovChainSG/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+// 1. Sentence Parser
+// 2. Word Input Counting
+// 3. Transition matrix
+// 4. Sentence Generator.
+
+
+int main()
+{
+	std::cout << "Hello, world!" << std::endl;
+	return 0;
+}
