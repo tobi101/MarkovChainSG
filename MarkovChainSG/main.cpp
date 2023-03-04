af42cc960f98a15883d67311aae1f16fa476290e
Sergey Reshetnikov <sreshetnikov101@gmail.com>
2023-03-04 04:35:42 +0300

    Implemented sentence parser

--- MarkovChainSG/main.cpp
+++ MarkovChainSG/main.cpp
@@ -1,26 +1,120 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
-	
-float** parser(std::string input_text)
+
+/*
+* 1. Парсер
+* In: Sentence
+* Out: Transition Matrix
+* 
+* -- unic_word - hash map for unic words
+* -- start_word - hash map for start words
+* -- end_word - hash map for end words
+*/
+
+
+// Text to Transition Matrix
+float** TextToMatrix(std::string input_text)
 {
-	std::string unic_word[100] = {};
-	std::string start_word[100] = {};
-	std::string end_word[100] = {};
-	
 	bool isStart = true;
 	bool isEnd = false;
 
-	float** transition_matrix = {};
+	std::string tempStr = "";
+	char nextChar = ' ';
 
+	std::unordered_map<std::string, int> unic_word;
+	std::unordered_map<std::string, int> start_word;
+	std::unordered_map<std::string, int> end_word;
+
+	// unic_word
 	for (int i = 0; i < input_text.length(); i++)
 	{
-		if (isStart == true)
+		nextChar = input_text[i + 1];
+
+		if ((input_text[i] >= 65 && input_text[i] <= 90) || 
+			(input_text[i] >= 97 && input_text[i] <= 122))
 		{
-			// ...write here...
-			isStart == false;
+			tempStr += input_text[i];
+
+		}
+		else if (
+			input_text[i] == '.' ||
+			input_text[i] == '!' ||
+			input_text[i] == '?')
+		{
+			if (end_word.find(tempStr) == end_word.end())
+			{
+				end_word.insert({ tempStr, 1 });
+			}
+			else
+			{
+				end_word[tempStr]++;
+			}
+
+			tempStr = "";
+
+			if (nextChar == ' ')
+			{
+				i++;
+			}
+
+			isStart = true;
+		}
+		else if (input_text[i] == ' ')
+		{
+			if (isStart)
+			{
+				if (start_word.find(tempStr) == start_word.end())
+				{
+					start_word.insert({ tempStr, 1 });
+				}
+				else
+				{
+					start_word[tempStr]++;
+				}
+
+				isStart = false;
+			}
+			else
+			{
+				if (unic_word.find(tempStr) == unic_word.end())
+				{
+					unic_word.insert({ tempStr, 1 });
+				}
+				else
+				{
+					unic_word[tempStr]++;
+				}
+			}
+
+			tempStr = "";
 		}
 	}
+
+	// Test cycles for unordered maps
+	std::cout << "unic_word: " << std::endl;
+	for (auto& pair : unic_word)
+	{
+		std::cout << pair.first << ": " << pair.second << std::endl;
+	}
+	std::cout << std::endl;
+
+	std::cout << "start_word: " << std::endl;
+	for (auto& pair : start_word)
+	{
+		std::cout << pair.first << ": " << pair.second << std::endl;
+	}
+	std::cout << std::endl;
+
+	std::cout << "end_word: " << std::endl;
+	for (auto& pair : end_word)
+	{
+		std::cout << pair.first << ": " << pair.second << std::endl;
+	}
+	std::cout << std::endl;
+
+	float** transition_matrix = {};
 
 	return transition_matrix;
 }
@@ -28,7 +122,9 @@ float** parser(std::string input_text)
 
 int main()
 {
+	std::string str = "";
+	std::getline(std::cin, str);
 
-	std::cout << "Hello, world!" << std::endl;
+	TextToMatrix(str);
 	return 0;
 }
